eaab245461a56b64d640bac746776fbb5397414a
AHMADSHEREEF <59100882+AHMADSHEREEF@users.noreply.github.com>
2022-11-18 19:08:39 +0300

    Add files via upload

---
+++ new.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<link rel="stylesheet" href="ahmad.css">
+<title>Ahmad's calculator</title>
+<link rel="stylesheet" href="style1.css">
+
+</head>
+<body>
+<form method="POST">
+<h1</دروستکرنا ژمارەکا نهێنی یا هەرەمەکی<h1 <
+<input name="number"> <br> <br>
+<input class="Ahmed" name="submit" type="submit" value="دروستکرن<"
+<br> <br>
+<h1>
+<? php
+if(isset($_POST['submit'])) {
+$zakho = $_POST['number'];
+$ahmad = "ahahahjqnaqjbnqjbjanqj! @#$%^&*(0987654321";
+echo substr(str_shuffle($ahmad),0, $zakho);
+}
+?>
+</h1>
+</form>
+</body>
+</html>
+
